1b5750a68e97ebf0647b26ce307fcff2c238e7cd
shenzhaonan <hellohuai@gmail.com>
2019-03-08 16:43:31 +0800

    init

---
+++ app/src/main/java/com/szn/unitysdkutil/UnityPlayerActivity.java
@@ -0,0 +1,137 @@
+package com.szn.unitysdkutil;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.Window;
+
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.unity3d.player.UnityPlayer;
+
+public class UnityPlayerActivity extends Activity
+{
+    protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    // Setup activity layout
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        mUnityPlayer = new UnityPlayer(this);
+        setContentView(mUnityPlayer);
+        mUnityPlayer.requestFocus();
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        //init admob
+        MobileAds.initialize(this, "ca-app-pub-4863732406306694~9539412435");
+
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+    }
+
+    @Override protected void onNewIntent(Intent intent)
+    {
+        // To support deep linking, we need to make sure that the client can get access to
+        // the last sent intent. The clients access this through a JNI api that allows them
+        // to get the intent set on launch. To update that after launch we have to manually
+        // replace the intent with the one caught here.
+        setIntent(intent);
+    }
+
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+        mUnityPlayer.quit();
+        super.onDestroy();
+    }
+
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+        mUnityPlayer.resume();
+    }
+
+    @Override protected void onStart()
+    {
+        super.onStart();
+        mUnityPlayer.start();
+    }
+
+    @Override protected void onStop()
+    {
+        super.onStop();
+        mUnityPlayer.stop();
+    }
+
+    // Low Memory Unity
+    @Override public void onLowMemory()
+    {
+        super.onLowMemory();
+        mUnityPlayer.lowMemory();
+    }
+
+    // Trim Memory Unity
+    @Override public void onTrimMemory(int level)
+    {
+        super.onTrimMemory(level);
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
+        {
+            mUnityPlayer.lowMemory();
+        }
+    }
+
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override public boolean dispatchKeyEvent(KeyEvent event)
+    {
+        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+            return mUnityPlayer.injectEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+    /*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
+
+    public void logFirebaseEvent(){
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "0815");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "Szn");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+    }
+}
